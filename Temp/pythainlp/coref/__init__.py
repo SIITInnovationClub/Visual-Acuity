6c394b414e25788269289966c1f2206299d6be67
ammaster10s <pawaris_avira@hotmail.com>
2024-08-23 15:35:22 +0700

    Test

--- Temp/pythainlp/coref/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-# SPDX-FileCopyrightText: 2016-2024 PyThaiNLP Project
-# SPDX-License-Identifier: Apache-2.0
-"""
-PyThaiNLP Coreference Resolution
-"""
-__all__ = ["coreference_resolution"]
-from pythainlp.coref.core import coreference_resolution
--- Temp/pythainlp/wsd/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-# SPDX-FileCopyrightText: 2016-2024 PyThaiNLP Project
-# SPDX-License-Identifier: Apache-2.0
-"""
-Thai Word Sense Disambiguation (WSD)
-"""
-__all__ = ["get_sense"]
-from pythainlp.wsd.core import get_sense
